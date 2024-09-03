997d45dbd830d5f7cdf7675e730dc46a72beaf4f
Mohamed Elgohary <mohamed.abdelazim.elgohary@gmail.com>
2024-09-03 04:17:25 +0300

    first login screen with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,230 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: LoginForm(),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(padding: EdgeInsets.only(top: 128.0)),
+        SizedBox(height: 16),
+        LoginHeader(),
+        SizedBox(height: 32),
+        WelcomeText(),
+        SizedBox(height: 16),
+        EmailField(),
+        SizedBox(height: 16),
+        PasswordField(),
+        SizedBox(height: 8),
+        ForgotPasswordButton(),
+        SizedBox(height: 16),
+        SignInButton(),
+        SizedBox(height: 16),
+        OrDivider(),
+        SizedBox(height: 16),
+        GoogleLoginButton(),
+        SizedBox(height: 16),
+        CreateAccountText(),
+        SizedBox(height: 16),
+      ],
+    );
+  }
+}
+
+class LoginHeader extends StatelessWidget {
+  const LoginHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Login here',
+        style: TextStyle(
+          color: Color(0xFF8A211F),
+          fontSize: 22,
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeText extends StatelessWidget {
+  const WelcomeText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Welcome back you’ve been missed!',
+        style: TextStyle(
+          fontSize: 16,
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.w400,
+          color: Colors.black54,
+        ),
+      ),
+    );
+  }
+}
+
+class EmailField extends StatelessWidget {
+  const EmailField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          borderSide: BorderSide(color: Color(0xFFE89122)),
+        ),
+        labelText: 'email',
+      ),
+    );
+  }
+}
+
+class PasswordField extends StatelessWidget {
+  const PasswordField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          borderSide: BorderSide(color: Color(0xFFE89122)),
+        ),
+        labelText: 'password',
+      ),
+    );
+  }
+}
+
+class ForgotPasswordButton extends StatelessWidget {
+  const ForgotPasswordButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: TextButton(
+        onPressed: () {},
+        child: const Text(
+          'Forgot password?',
+          style: TextStyle(
+            decoration: TextDecoration.underline,
+            color: Color(0xFF8A211F),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFF8A211F),
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+      ),
+      child: const Text(
+        'Sign In',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class OrDivider extends StatelessWidget {
+  const OrDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(
+          child: Divider(
+            color: Colors.black38,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text('or'),
+        ),
+        Expanded(
+          child: Divider(
+            color: Colors.black38,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class GoogleLoginButton extends StatelessWidget {
+  const GoogleLoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.grey[300],
+        side: BorderSide(color: Colors.grey[700]!),
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+      ),
+      child: const Text(
+        'Login With Google',
+        style: TextStyle(
+          color: Color(0xFF8A211F),
+        ),
+      ),
+    );
+  }
+}
+
+class CreateAccountText extends StatelessWidget {
+  const CreateAccountText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Create new account',
+        style: TextStyle(
+          color: Color(0xFF8A211F),
         ),
       ),
     );
